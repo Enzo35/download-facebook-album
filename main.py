628cc5da4ed9061d7faae520615a90e8f7c99d2e
Enzo Gusmao <enzogusmaobezerra@outlook.com>
2024-03-08 18:30:54 -0300

    futher added verification pf status code

--- main.py
+++ main.py
@@ -3,7 +3,14 @@ import sys
 import requests
 from bs4 import BeautifulSoup
 
+BASE_FACEBOOK_URL = "https://www.facebook.com/"
+
 url = input("Input Facebook Album URL: \n")
+if url[:len(BASE_FACEBOOK_URL)] != BASE_FACEBOOK_URL:
+    print("Page not recognized as Facebook, problems may occuor.")
+    print("Stoping script...")
+    sys.exit(0)
+
 print("\n")
 
 response = requests.get(url)
