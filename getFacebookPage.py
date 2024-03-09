2d9ae0eba44e9dd20ff454d90d36b3398f74df7d
Enzo Gusmao <enzogusmaobezerra@outlook.com>
2024-03-09 16:07:06 -0300

    Added selenium

---
+++ getFacebookPage.py
@@ -0,0 +1,24 @@
+# Code credits to Driftr95, at https://stackoverflow.com/questions/74100308/facebook-scraping-python
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+def linkToSoup_selenium(l, ecx=None):
+    try:
+        driver = webdriver.Chrome('chromedriver.exe')
+        # I copy chromedriver.exe to every folder with py file that will need to scrape with selenium
+
+        driver.get(l)
+        if ecx:
+            WebDriverWait(driver, 25).until(
+                EC.visibility_of_all_elements_located((By.XPATH, ecx)))
+
+        lSoup = BeautifulSoup(driver.page_source, 'html.parser')
+        driver.close()
+        del driver
+        return lSoup
+    except Exception as e:
+        print(e)
+        return None
